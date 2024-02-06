25a96d6bb5d48c015e1413718940cbafdee40480
Jon <jon.herman@ionic.io>
2024-02-06 10:40:33 -0500

    add cap platforms and iv deps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'jon-react-iv-test',
+  appId: 'io.ionic.gettingstartediv',
+  appName: 'iv-getting-started',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
